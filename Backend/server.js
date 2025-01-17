096701fd2eaaf1b1fbff283bf29e7a8133168b8e
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-01-17 15:11:24 +0500

    fixing some mismatches

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,29 @@
-import express from "express"; 
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import router from "./Routes/contactForm.js";
 import connectToMongo from "./Config/db.js";
 import cors from "cors";
 
-connectToMongo();
 dotenv.config();
-const app = express(); 
+
+connectToMongo();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: "http://localhost:8080",
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
-  };
-  
-  app.use(cors(corsOptions));
+  origin: process.env.CLIENT_ORIGIN || "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(express.json());   
-app.use('/api',router)
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.use("/api", router);
+
+
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`); 
-}); 
- 
+  console.log(`Server started on port ${port}`);
+});
